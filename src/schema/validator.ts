81693b82184f4d65fe476c73611dae60853f7135
Gautham Ramachandran <gautham2r@gmail.com>
2020-04-22 22:07:49 +0200

    restructure stuff

--- src/schema/validator.ts
+++ src/schema/validator.ts
@@ -1,5 +1,5 @@
 import Ajv, { ErrorObject, ValidationError } from 'ajv';
-import * as jsonSchemaDraft04 from 'ajv/lib/refs/json-schema-draft-04.json';
+import * as jsonSchemaDraft04 from 'ajv/lib/refs/json-schema-draft-04';
 import * as openAPI3Schema from './resources/v3.0/schema.json';
 import { OpenAPIV3 } from 'openapi-types';
 import { Either, left, right } from 'fp-ts/lib/Either';
